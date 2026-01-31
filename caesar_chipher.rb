c319733ad314b9c65bd8ee704ee8b28c550e632f
spinner5 <166389859+antzax@users.noreply.github.com>
2026-01-31 21:02:37 +0200

    ok

--- caesar_chipher.rb
+++ caesar_chipher.rb
@@ -1,11 +1,14 @@
 def caesar_cipher(string, shift)
-    string_bytes = string.bytes
-    
-    p string_bytes
-
-    string_bytes = string_bytes.each {|b| b += 5}
-
-    p string_bytes
+    shifted_chars = string.chars.map do |char|
+        base = char.ord < 91 ? 65 : 97
+        if char.ord.between?(65, 90) || char.ord.between?(97,122)
+            (((char.ord - base + shift) % 26) + base).chr # normalize base then shift it with modulo and add back base
+        else
+            char
+        end
+    end
+    shifted_chars.join
 end
 
-caesar_cipher("anton veijola", 5)
+puts caesar_cipher("What a string!", 5)
+p caesar_cipher("What a string!", 5)
